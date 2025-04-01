afc540436772b04a6cde4ba76f82b9adb1f900c7
Nick Kiselev <nick@mar.so>
2025-04-02 01:04:24 +0200

    #8 center buttons

--- src/components/menu_header/MenuToggleButton.tsx
+++ src/components/menu_header/MenuToggleButton.tsx
@@ -5,16 +5,14 @@ import React from "react"
 interface MenuToggleButtonProps {
   isOpen: boolean
   onClick: () => void
-  className?: string
 }
 
 const MenuToggleButton: React.FC<MenuToggleButtonProps> = ({
   isOpen,
   onClick,
-  className,
 }) => {
   return (
-    <button className={`menu-toggle-button ${className}`} onClick={onClick}>
+    <button className={`menu-toggle-button`} onClick={onClick}>
       <div className={`line top ${isOpen ? "open" : ""}`}></div>
       <div className={`line middle ${isOpen ? "open" : ""}`}></div>
       <div className={`line bottom ${isOpen ? "open" : ""}`}></div>
