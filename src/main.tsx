46c56c2c9d5e7bd57f2362f9c6b06632a07f8858
Aquil <nindzy142@yandex.ru>
2025-04-04 20:09:23 +0300

    Сделал по дефолту файл main.tsx. Переименовал файлы на EventsSection. Json не трогаю, потому что до этого привел его в состояние, которое стояло по дефолту в init ветке. Убрал вызов метода EventSection из main, в нем по дефолту вызывается App.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Events_section from './sections/Events_section.tsx'
+import  App from './App.tsx' 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Events_section />
+    <App/>
   </StrictMode>,
 )
