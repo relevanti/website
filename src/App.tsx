8511708283dcddef88ad72acfc88f1fa6b96ccab
Nick Kiselev <nick@mar.so>
2025-04-18 15:19:32 +0200

    #7 footer section

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Header from "./components/menu_header/Header"
 import { MobileProvider } from "./context/MobileContext"
 import AboutUsCards from "./sections/about_us_cards/AboutUsCards"
 import AboutUs from "./sections/about_us/AboutUs"
+import FooterSection from "./sections/footer/FooterSection"
 import FrontSection from "./sections/front_section/FrontSection"
 
 const App: React.FC = () => {
@@ -18,6 +19,7 @@ const App: React.FC = () => {
         <AboutUs />
         <AboutUsCards />
         <EventsSection />
+        <FooterSection />
       </div>
     </MobileProvider>
   )
---
+++ src/sections/footer/FooterSection.tsx
@@ -0,0 +1,31 @@
+import "./FooterSection.css"
+
+import React from "react"
+
+const FooterSection: React.FC = () => {
+  return (
+    <section id="join" className="footer-section">
+      <div className="enroll-section">
+        <h2 className="footer-header">Мы ждали тебя</h2>
+        <p className="footer-paragraph">
+          1. Заполни форму по ссылке
+          <br />
+          2. Обсуди концепцию на собеседовании
+          <br />
+          3. Покажи заинтересованность действием
+        </p>
+        <a href="https://relevanti.org/apply" className="join-button">
+          Вступить
+        </a>
+      </div>
+      <div className="footer-content">
+        <div className="contact-details">
+          <p>contact@relevanti.org</p>
+          <p>© 2025 Сообщество Релеванты</p>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default FooterSection
