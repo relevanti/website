9a7ad49ec90609352b57f1127b1a528e94b3ff23
Nick Kiselev <nick@mar.so>
2025-04-18 22:48:58 +0200

    more adaptive header

--- src/components/horizontal_logo/HorizontalLogo.d.ts
+++
@@ -1,8 +0,0 @@
-import "./HorizontalLogo.css";
-import * as React from "react";
-interface HorizontalLogoProps {
-    expand: boolean;
-    size: number;
-}
-declare const HorizontalLogo: React.FC<HorizontalLogoProps>;
-export default HorizontalLogo;
